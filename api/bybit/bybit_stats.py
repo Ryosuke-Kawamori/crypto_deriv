9f4ae3d3016c048855bfbd10d9a5d61dfabbf9e1
Ryosuke-Kawamori <at01ryo@gmail.com>
2024-05-04 03:22:37 +0900

    pnl caclculation

--- api/bybit/bybit_stats.py
+++ api/bybit/bybit_stats.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from datetime import datetime
 
-def pnl(fig_path: str):
+def pnl(fig_path: str='pnl.png'):
     bybit = Bybit(os.getenv('BYBIT_APIKEY'), os.getenv('BYBIT_SECRET'))
     response = bybit.send_request('GET', 'private', target_path='/v5/execution/list', params={'category':'option', 'limit': '50'})
     trades = pd.DataFrame(response.json().get('result').get('list'))
@@ -31,7 +31,7 @@ def pnl(fig_path: str):
     pnl = (
         pnl
             .assign(KnockIn = lambda df: ((df['CP']=='P') & (df['K']>=df['deliveryPrice'])) | ((df['CP']=='C') & (df['K']<=df['deliveryPrice'])))
-            .assign(TradePnL = lambda df: (df['execPrice']*df['orderQty']))
+            .assign(TradePnL = lambda df: (df['execPrice']*df['execQty']))
             .assign(DeliverPnL = lambda df: df['execQty']*(df['deliveryPrice']-df['K'])*(df['CP'].replace({'C': '1', 'P': '-1'}).astype(int))*(df['side'].replace({'Sell': '-1', 'Buy': '1'}).astype(int)))
             .assign(DeliverPnL = lambda df: df['DeliverPnL'].mask(~df['KnockIn'], 0))
             .assign(PnL = lambda df: df['TradePnL'] + df['DeliverPnL'] - df['execFee'])
